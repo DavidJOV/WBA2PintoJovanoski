44eac3d7ccc0261c05742af9f42f82342f3c29a9
Paulo Pinto <33468773+paulovpinto@users.noreply.github.com>
2018-04-12 09:28:06 +0200

    3.Aufgabe done

--- Kickoff/kickoff.js
+++ Kickoff/kickoff.js
@@ -1,17 +1,22 @@
-var	fs	=	require('fs');
+var fs = require('fs');
 var chalk = require('chalk');
 var obj;
 
-fs.readFile('staedte.json', "utf8",	function(err,data)	{
+fs.readFile('staedte.json', 'utf8', function (err, data) {
+
   if (err) throw err;
+  obj = JSON.parse(data);
+  
+  obj.cities.sort(function (a, b) {
+    return a.population - b.population;
+    });
 
-  var obj = JSON.parse(data);
-
+fs.writeFile("staedte_sortiert.json", JSON.stringify(obj));
   for (arraynumber in obj.cities)
+
   console.log(chalk.yellow('\n','name: ',obj.cities[arraynumber].name)
               ,chalk.red('\n','country: ',obj.cities[arraynumber].country)
               ,chalk.green('\n','population: ',obj.cities[arraynumber].population)
               ,'\n','\n','-------------');
 
-
-});
+	});
