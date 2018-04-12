7f0f08b152f2f8c933f7430e2c11b3295021bc26
Paulo Pinto <33468773+paulovpinto@users.noreply.github.com>
2018-04-12 09:15:57 +0200

    2.Aufgabe done

--- Kickoff/kickoff.js
+++ Kickoff/kickoff.js
@@ -1,4 +1,5 @@
 var	fs	=	require('fs');
+var chalk = require('chalk');
 var obj;
 
 fs.readFile('staedte.json', "utf8",	function(err,data)	{
@@ -7,9 +8,10 @@ fs.readFile('staedte.json', "utf8",	function(err,data)	{
   var obj = JSON.parse(data);
 
   for (arraynumber in obj.cities)
-  console.log('\n','name: ',obj.cities[arraynumber].name
-              ,'\n','country: ',obj.cities[arraynumber].country
-              ,'\n','population: ',obj.cities[arraynumber].population
+  console.log(chalk.yellow('\n','name: ',obj.cities[arraynumber].name)
+              ,chalk.red('\n','country: ',obj.cities[arraynumber].country)
+              ,chalk.green('\n','population: ',obj.cities[arraynumber].population)
               ,'\n','\n','-------------');
+
 
 });
