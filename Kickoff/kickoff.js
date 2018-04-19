f00d4af7d612401d63b83ef92420424b162e9a94
Paulo Pinto <33468773+paulovpinto@users.noreply.github.com>
2018-04-19 12:09:13 +0200

    kickoff fixed and testing git function of visual studio code :)

--- Kickoff/kickoff.js
+++ Kickoff/kickoff.js
@@ -6,17 +6,28 @@ fs.readFile('staedte.json', 'utf8', function (err, data) {
 
   if (err) throw err;
   obj = JSON.parse(data);
-  
+
   obj.cities.sort(function (a, b) {
     return a.population - b.population;
-    });
+}
+);
 
-fs.writeFile("staedte_sortiert.json",'utf8', JSON.stringify(obj));
-  for (arraynumber in obj.cities)
+}
+);
 
-  console.log(chalk.yellow('\n','name: ',obj.cities[arraynumber].name)
-              ,chalk.red('\n','country: ',obj.cities[arraynumber].country)
-              ,chalk.green('\n','population: ',obj.cities[arraynumber].population)
-              ,'\n','\n','-------------');
+//fs.writeFile("staedte_sortiert.json",'utf8', JSON.stringify(obj));
+fs.writeFile("staedte_sortiert.json",'utf8', function (err, data){
+  
+    if (err) throw err;
+    JSON.stringify(obj);
 
-	});
+    for (arraynumber in obj.cities)
+    console.log(chalk.yellow('\n','name: ',obj.cities[arraynumber].name)
+                ,chalk.red('\n','country: ',obj.cities[arraynumber].country)
+                ,chalk.green('\n','population: ',obj.cities[arraynumber].population)
+                ,'\n','\n','-------------');
+
+}
+);
+
+
