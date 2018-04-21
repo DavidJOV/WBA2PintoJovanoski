08667bb9b11861f5d973065056dadf72d31917b4
Paulo Pinto <33468773+paulovpinto@users.noreply.github.com>
2018-04-21 14:12:59 +0200

    working on it

--- Kickoff/ex2.js
+++ Kickoff/ex2.js
@@ -1,23 +1,44 @@
 var fs = require('fs');
+
+var chalk = require('chalk');
+
 var obj;
+
 var obj2;
+
+var obj3;
 
 fs.readFile('staedte.json', 'utf8', function (err, data) {
 
+    fs.readFile("mehr_staedte.json",'utf8',	function(err,	data2)	{
+
     if (err) throw err;
+
     obj = JSON.parse(data);
 
-    console.log('read1');
-//for(arraynumber in obj.cities)
-//console.log('\n','name:',obj.cities[arraynumber].name,'\n','country: ', obj.cities[arraynumber].country);
 });
 
-fs.readFile('mehr_staedte.json', 'utf8', function (err, data2) {
-
-    if (err) throw err;
     obj2 = JSON.parse(data2);
-    console.log('2read');
-//for(arraynumber in obj.cities)
-//console.log('\n','name:',obj.cities[arraynumber].name,'\n','country: ', obj.cities[arraynumber].country);
 
 });
+
+obj3 = Array.from(obj.cities).concat(Array.from(obj2.cities));
+
+    obj3.sort(function (a, b) {
+        return a.population - b.population;
+    });
+
+    fs.writeFile("concatstaedte.json",'utf8', function (err, data){
+
+        if (err) throw err;
+        JSON.stringify(obj3);
+    
+        for (arraynumber in obj3.cities)
+        console.log(chalk.yellow('\n','name: ',obj3.cities[arraynumber].name)
+                    ,chalk.red('\n','country: ',obj3.cities[arraynumber].country)
+                    ,chalk.green('\n','population: ',obj3.cities[arraynumber].population)
+                    ,'\n','\n','-------------');
+    
+    }
+    );
+   
