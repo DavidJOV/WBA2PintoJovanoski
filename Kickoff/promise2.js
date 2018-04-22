52277fe66ad5aea6b927b095400ae151e7370d1a
Paulo Pinto <33468773+paulovpinto@users.noreply.github.com>
2018-04-22 18:48:33 +0200

    working on promises

--- Kickoff/promise2.js
+++ Kickoff/promise2.js
@@ -1,6 +1,10 @@
 var fs = require('fs');
 
+var chalk = require('chalk');
+
 var obj;
+
+var obj2;
 
 var p = new Promise(function(resolve, reject) {
 	
@@ -10,15 +14,25 @@ var p = new Promise(function(resolve, reject) {
 		obj = JSON.parse(data);
 		console.log('read');
 
+		obj.cities.sort(function (a, b) {
+			return a.population - b.population;
+		  }
+		);
+
 		if (err) 
 			reject(err); 
 		else 
 			resolve(data);
 	});
+
 });
 
 p.then(function() { 
-	console.log(obj);
+
+	for(i in obj.cities){
+		console.log(chalk.red('\n',obj.cities[i].name));
+	}
+
 }).catch(function(err) {
 	console.log(err);
-})
+})	
