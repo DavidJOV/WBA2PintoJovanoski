fcc749ee6875db6fb3d71435e5477b20e0b8bdbf
David Jovanoski (inf491) <david.jovanoski@smail.th-koeln.de>
2018-04-24 21:39:32 +0200

    Promise Aufgabe fertig.

--- Kickoff/Aufgabenblatt2.2.js
+++ Kickoff/Aufgabenblatt2.2.js
@@ -4,40 +4,73 @@ var contents;
 var contents2;
 var contents3;
 
- fs.readFile("staedte.json",'utf8',	function(err,	data1)	{
-    fs.readFile("mehr_staedte.json",'utf8',	function(err,	data2)	{
-  if (err) throw err;
+var p1 = new Promise( function(resolve,reject){
+setTimeout(function(){
+  fs.readFile("staedte.json",'utf8',	function(err,	data1)	{
+
+    if (err) throw err;
     contents = JSON.parse(data1);
-  contents.cities.sort(function (a,b){
-     return a.population - b.population;
-  });
-
-  contents2 = JSON.parse(data2);
-    contents2.cities.sort(function (a,b){
-       return a.population - b.population;
-    });
-    
-   
- contents3 = Array.from(contents.cities).concat(Array.from(contents2.cities));
- 
-contents3.sort(function (a,b){
-    return a.population - b.population;
- });
- var dataWrite = JSON.stringify(contents3)
-    
-    fs.writeFile("staedteneuA2.1.json", JSON.stringify(contents3)); 
-     
-   
-   var i;
-
-   for(i=0;i< contents3.length;i++){
-    
-    console.log(chalk.blue('\n','name: ',contents3[i].name),
-    chalk.red('\n country: ',contents3[i].country),
-    chalk.yellow( '\n population: ',contents3[i].population),
-            '\n --------------');
-}
-
+    if (err) 
+			reject(err); 
+		else 
+      resolve(data1);
+      console.log("FILE1")
 
 })
+
+
+
+},500)
+  });
+
+//---------------------------------------------------------------------------
+
+var p2 = new Promise( function(resolve,reject){
+
+  fs.readFile("mehr_staedte.json",'utf8',	function(err,	data2)	{
+    if (err) throw err;
+      contents2 = JSON.parse(data2);
+      if (err) 
+			reject(err); 
+		else 
+      resolve(data2);
+      console.log("FILE2")
+
+})});
+
+//---------------------------------------------------------------------------
+
+Promise.all([p1,p2]).then(function(){
+  contents3 = Array.from(contents.cities).concat(Array.from(contents2.cities));
+  console.log('Staedte verbunden');
+
+},function(err){
+  console.log(err);
+}).then(function(){
+  console.log('Staedte sortiert');
+  contents3.sort(function (a,b){
+    return a.population - b.population;
+    
  });
+}).then(function(){
+  var dataWrite = JSON.stringify(contents3)
+    
+    fs.writeFile("staedteneuA2.2.json", JSON.stringify(contents3));
+    console.log('Staedte in File geschrieben');
+
+
+}).then(function(){
+  var i;
+
+  for(i=0;i< contents3.length;i++){
+   
+   console.log(chalk.blue('\n','name: ',contents3[i].name),
+   chalk.red('\n country: ',contents3[i].country),
+   chalk.yellow( '\n population: ',contents3[i].population),
+           '\n --------------');
+
+}}).catch(function(err){
+  console.log(err);
+});
+
+
