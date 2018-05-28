8961d88c878adbe0b24fb68419148aa3d2504a9c
Paulo Pinto <33468773+paulovpinto@users.noreply.github.com>
2018-05-28 09:39:54 +0200

    more express routing lines of code implemented

--- app/app.js
+++ app/app.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 
+//Route definition takes the following structure:
+//app.METHOD(PATH, HANDLER)
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/',(req, res) => res.send('Got a POST request'))
+
+app.put('/user', (req,res) => res.send('Got a PUT request'))
+
+app.delete('/user', (req,res) => res.send('Got a DELETE request at /user'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
