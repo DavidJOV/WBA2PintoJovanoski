7c435b9dadb804f1c6e3b8ac11a1208842b68d04
Paulo Pinto <33468773+paulovpinto@users.noreply.github.com>
2018-05-28 12:42:26 +0200

    formatted

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-var users = require('./users/users.js');
+const users = require('./users/users.js');
 app.use('/users', users );
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- app/users/users.js
+++ app/users/users.js
@@ -2,13 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', (req, res) => {
+
    console.log('posted');
    res.send('POST route on Users.');
+   
 });
 
 router.get('/', (req, res) => {
+
     console.log('got');
     res.send('GET route on Users.');
+
  });
 
 module.exports = router;
