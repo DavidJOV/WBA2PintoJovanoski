c8b47633ecb600ea4d2c8e5fa2850039c7d4dc7b
Paulo Pinto <33468773+paulovpinto@users.noreply.github.com>
2018-05-28 12:13:45 +0200

    routing

--- app/app.js
+++ app/app.js
@@ -1,15 +1,7 @@
 const express = require('express')
 const app = express()
 
-//Route definition takes the following structure:
-//app.METHOD(PATH, HANDLER)
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.post('/',(req, res) => res.send('Got a POST request'))
-
-app.put('/user', (req,res) => res.send('Got a PUT request'))
-
-app.delete('/user', (req,res) => res.send('Got a DELETE request at /user'))
+var users = require('./users/users.js');
+app.use('/users', users );
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
---
+++ app/users/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', (req, res) => {
+   console.log('posted');
+   res.send('POST route on Users.');
+});
+
+router.get('/', (req, res) => {
+    console.log('got');
+    res.send('GET route on Users.');
+ });
+
+module.exports = router;
