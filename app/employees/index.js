64671738d76a7bd1cf2b2505d4b680ba2fa074d1
DavidJOV <davidjova94@gmail.comv>
2018-06-12 00:18:33 +0200

    Tryed some sorting, didnt worked yet

--- app/employees/index.js
+++ app/employees/index.js
@@ -7,6 +7,7 @@ var randomLocation = require('random-location');
 var obj;
 var employeesArray;
 var distance = require('../distances/distancer.js');
+var distanceArray;
 
 
 
@@ -41,11 +42,66 @@ function generateEmployeesLocation(){ // generating random location for each emp
 
     });
 });}
+/*
+function getNearestEmployee(custmorAdress){
+var p = new Promise(function(resolve, reject) {
+    fs.readFile('employees.json','utf8', function(err, data){
+        employeesArray = Array.from (JSON.parse(data));
+ 
+        if (err) 
+        reject(err); 
+    else 
+        resolve(data);
+});
+
+});
+
+
+p
+.then(function(){
+     distanceArray = [employeesArray.length];
+      
+    for(i=0;i< employeesArray.length;i++){
+       var origin = employeesArray[i].location.latitude +","+ employeesArray[i].location.longitude;
+       overGive(i,function(i){
+
+
+        distance.getDistance(origin,custmorAdress,function(distanceToCustmor){
+            
+             distanceArray[i]= {
+                distance: distanceToCustmor,
+                employeeName: employeesArray[i].name,
+                employeeLastname: employeesArray[i].lastname,
+                employeeSpezialgebiet: employeesArray[i].Spezialgebiet
+                
+             }
+
+            });}
+
+);}
+
+})
+.then(function(){
+
+    distanceArray.sort(function(a,b){
+        return b.distance - a.distance;});
     
+}).then(function(){
+    console.log(distanceArray[0].distance);
+})
+
+
+
+}
+*/
+
+
+
 function getNearestEmployee(custmorAdress){
     fs.readFile('employees.json','utf8', function(err, data){
         employeesArray = Array.from (JSON.parse(data));
         var distanceArray = [employeesArray.length];
+      
         for(i=0;i< employeesArray.length;i++){
            var origin = employeesArray[i].location.latitude +","+ employeesArray[i].location.longitude;
            overGive(i,function(i){
@@ -55,17 +111,60 @@ function getNearestEmployee(custmorAdress){
                
                 distanceArray[i]= {
                    distance: distanceToCustmor,
-                   employee: employeesArray[i].name
+                   employeeName: employeesArray[i].name,
+                   employeeLastname: employeesArray[i].lastname,
+                   employeeSpezialgebiet: employeesArray[i].Spezialgebiet
                    
                 }
+                if(distanceArray[i-1]!=undefined){
+                    let temp;
+                    if(distanceArray[i-1].distance > distanceArray[i].distance){
+                        temp = distanceArray[i-1];
+                        distanceArray[i-1] = distanceArray[i];
+                        distanceArray[i]=temp;
+                    }
+                }
+                console.log(JSON.stringify(distanceArray[0]));
+                //console.log("Mitarbeiter: "+i+" "+JSON.stringify(distanceArray[i].employeeName)+" "+JSON.stringify((distanceArray[i].distance)/1000)+" KM"); //muss noch sortiert werden
+               /*sortIT(distanceArray,function(distanceArray){
+
+                  distanceArray =  distanceArray.sort(function(a,b){
+                        return b.distance - a.distance;
+                    });
+                    
+                });
+                ------------------------------------------------------
+                var sortedDistanceArray = [distanceArray.length];
+                distanceArray.sort(function(a,b){
+                    return b.distance - a.distance;});
+                for(var j=0;j<distanceArray.length;j++){
+                    sortedDistanceArray[j] = distanceArray[j];
+                    
+                }
+                ------------------------------------------------------------
+                console.log(sortedDistanceArray);
+              
+                //console.log("Mitarbeiter: "+i+" "+ JSON.stringify(distanceArray[i]));//muss noch sortiert werden
+               
+               */
+               // console.log("Mitarbeiter: "+JSON.stringify(distanceArray)); //muss noch sortiert werden
                 
-                console.log("Mitarbeiter: " +i+ " "+JSON.stringify(distanceArray[i])); //muss noch sortiert werden
                 
             });
-        });
+            
+            
+        }
+    
+    );
 
 
-        }});}
+        }
+         
+    });}
+
+function sortIT(array,sortIT){  // not used yet
+    sortIT(array);
+}        
 
 function overGive(i,callback){
     callback(i);
